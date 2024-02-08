edef1f8b093d05a1371bd1a909b4033eee85390d
Baptiste ZLOCH <bzloch@hotmail.fr>
2024-02-08 11:20:20 +0100

    Update run_for_linux.sh

--- run_for_linux.sh
+++ run_for_linux.sh
@@ -1,2 +1,6 @@
+# Create the out directory if not exists
+[ -d "out" ] || mkdir "out"
+# Compiling the project
 g++ -I./include -std=c++20 -Wall -g -O3 src/game_of_life.cpp src/cell.cpp src/main.cpp  -o out/main.out -march=native -fopenmp 
+# Running the project with rows cols n_generation arguments.
 out/main.out 50 50 100
